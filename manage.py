6f372b760da97494b4bfba5ac237c553449a1332
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2018-10-31 14:08:40 +0100

    first commit

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quickbooks.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gpstracker.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
